342385ad9016dc0f2603127a60500f066e23cee8
NiyatiPriyani <niyatipriyani@gmail.com>
2026-03-28 16:40:17 +0530

    add vector db client and repo

---
+++ backend/internal/db/vector/client.go
@@ -0,0 +1,30 @@
+package vector
+
+import (
+	"context"
+	"os"
+
+	pinecone "github.com/pinecone-io/go-pinecone/v2/pinecone"
+)
+
+type Client struct {
+	Index *pinecone.IndexConnection
+}
+
+func NewClient() (*Client, error) {
+	apiKey := os.Getenv("PINECONE_API_KEY")
+
+	pc, err := pinecone.NewClient(pinecone.Config{
+		ApiKey: apiKey,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	index := pc.Index("news-index")
+
+	return &Client{
+		Index: index,
+	}, nil
+}
